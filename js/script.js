7d443a00eeb87db817d1e81fbd6f7e7f2215ec2f
nkukar <nkukar@inbox.ru>
2021-12-09 01:49:09 +0300

    endResponsive

--- js/script.js
+++ js/script.js
@@ -9,15 +9,19 @@ function scrollTo(element) {
 
 const buttonScroll = document.querySelectorAll('.menu__list-link');
 const positionBlock = document.querySelectorAll('section');
-
+const menuBtn = document.querySelector('.burger'),
+		menuList =  document.querySelector('.menu__list');
 buttonScroll.forEach (item => {
 	item.addEventListener('click', (e) => {
 		e.preventDefault();
-		
+		if (menuList.classList.contains('menu__list--active')) {
+			document.querySelector('.menu__list').classList.remove('menu__list--active');
+			document.body.style.overflow ="";
+		}
 		positionBlock.forEach(block => {
-			console.log(block.id);
 			if(item.hash == (`#${block.id}`)) {
 				scrollTo(block)
+				
 			}
 			
 		})
@@ -27,17 +31,15 @@ buttonScroll.forEach (item => {
 
 // Show menu
 
-const menuBtn = document.querySelector('.burger'),
-		menuList =  document.querySelector('.menu__list');
+
 
 menuBtn.addEventListener('click', () => {
 	menuList.classList.toggle('menu__list--active');
 	if (menuList.classList.contains('menu__list--active')) {
-		console.log(document.body);
 		document.body.style.overflow ='hidden';
-		
 	}else  {
 		document.body.style.overflow ="";
 		
 	}
 })
+
