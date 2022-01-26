5b7022e5a0deb965cf1005fba7797536bcc580bc
nkukar <nkukar@inbox.ru>
2022-01-26 07:21:01 +0300

    link firebase

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@
 const modalBtns = document.querySelectorAll('.modal-active'),
 		modalContent = document.querySelector('.modal');
 
-console.log(modalBtns);
 
 modalBtns.forEach(item => {
 	item.addEventListener('click', () => {
@@ -123,7 +122,76 @@ if(screen.width < 1020) {
 	const blockMargin = document.querySelector('.about__items').offsetLeft;
 	sliderMargin1.style.paddingLeft = blockMargin+'px';
 
-	console.log(blockMargin);
+
+
+// Работа с FireBase
+
+
+function setCookie(name,value,days) {
+	if (days) {
+	  var date = new Date();
+	  date.setTime(date.getTime()+(days*24*60*60*1000));
+	  var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+function getCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+	  var c = ca[i];
+	  while (c.charAt(0)==' ') c = c.substring(1,c.length);
+	  if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+function delCookie(name) {
+	setCookie(name,"",-1);
+}
+if (!(getCookie("id"))) {
+	function uuidv4() {
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+			var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+			return v.toString(16);
+		});
+	};
+	const userId = uuidv4();
+	setCookie("id",userId,365);
+}
+
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDFFnvfZMbENuJb-COBC2rPXNU9AzOyJWc",
+	authDomain: "sferus360-aee4a.firebaseapp.com",
+	projectId: "sferus360-aee4a",
+	storageBucket: "sferus360-aee4a.appspot.com",
+	messagingSenderId: "562847796404",
+	appId: "1:562847796404:web:cfc9ae020a2049f61f5be1",
+	measurementId: "G-W7ZT9RZXGJ",
+	databaseURL: "https://sferus360-aee4a-default-rtdb.firebaseio.com",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+
+const userCookie = getCookie('id');
+
+const form = document.querySelector('form'),
+		formInputAll = form.querySelectorAll('.modal-input'),
+		formCheckbox = form.querySelector('#label1'),
+		formBtn = form.querySelector('button');
+
+
+// formBtn.addEventListener('click', () => {
+
+// })
+
+
+// const db = firebase.database().ref('message/' + userCookie).push('lox')
+
+
 
 
 
