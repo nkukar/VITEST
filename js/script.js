b4cc3625895016414918b20af8ef3bde6c4e5a37
nkukar <nkukar@inbox.ru>
2022-01-26 05:18:56 +0300

    Sferus add Responsive

--- js/script.js
+++ js/script.js
@@ -42,18 +42,91 @@ function hide () {
 };
 
 
-function showContent (i) {
+function showContent (i = 0) {
 	sliderContents[i].style.display = 'block';
 	sliderBtns[i].classList.add('products-btn--active');
 	
 }
-showContent(0);
+showContent();
 sliderBtns.forEach ((item,index) => {
 	item.addEventListener('click', () => {
 		hide();
 		showContent(index)
 	})
 })
+
+// Работа стрелок
+if(screen.width < 1020) {
+	const arrowLeft = document.querySelector('.arrow-left'),
+	arrowRight = document.querySelector('.arrow-right'),
+	arrowAll = document.querySelectorAll('.products-btn--arrow');
+	let count = 0;
+
+	if(count == 0) {
+		arrowLeft.style.display = 'none';
+	}
+	function hideArr () {
+		sliderBtns.forEach(item => {
+			item.style.display = 'none';
+
+		})
+		sliderContents.forEach(items => {
+			items.style.display = 'none';
+		})
+
+	};
+	function showContentArr (i = 0) {
+		sliderContents[i].style.display = 'block';
+		sliderBtns[i].style.display = 'block';
+		
+	}
+	hideArr ();
+	showContentArr(0)
+
+	arrowAll.forEach(item => {
+		item.addEventListener('click', (e) => {
+			hideArr ();
+			if(e.target.classList.contains('arrow-right')) {
+				count++;
+				showContentArr(count)
+				arrowAll.forEach(arrow => {
+					arrow.style.display = 'block';
+				})
+				if(count == (sliderContents.length -1)) {
+					arrowRight.style.display = 'none';
+				}
+			}
+			if(e.target.classList.contains('arrow-left')) {
+				count--;
+				showContentArr(count)
+				arrowAll.forEach(arrow => {
+					arrow.style.display = 'block';
+				})
+				if(count == 0) {
+					arrowLeft.style.display = 'none';
+				}
+			}
+			
+		})
+	})
+}
+
+
+
+
+// Отступ блока 
+
+	const blockMargin1 = window.screen.availWidth;
+	const sliderMargin1 = document.querySelector('.slider__items');
+	sliderMargin1.style.width = blockMargin1+'px';
+
+	const blockMargin = document.querySelector('.about__items').offsetLeft;
+	sliderMargin1.style.paddingLeft = blockMargin+'px';
+
+	console.log(blockMargin);
+
+
+
 
 
 
