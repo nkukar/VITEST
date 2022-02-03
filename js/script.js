9f25f2836ed75243d2df6dff0e589a31c1a20ff1
nkukar <nkukar@inbox.ru>
2022-02-03 03:13:48 +0300

    add new resposive

--- js/script.js
+++ js/script.js
@@ -55,72 +55,72 @@ sliderBtns.forEach ((item,index) => {
 })
 
 // Работа стрелок
-if(screen.width < 1020) {
-	const arrowLeft = document.querySelector('.arrow-left'),
-	arrowRight = document.querySelector('.arrow-right'),
-	arrowAll = document.querySelectorAll('.products-btn--arrow');
-	let count = 0;
+// if(screen.width < 1020) {
+// 	const arrowLeft = document.querySelector('.arrow-left'),
+// 	arrowRight = document.querySelector('.arrow-right'),
+// 	arrowAll = document.querySelectorAll('.products-btn--arrow');
+// 	let count = 0;
 
-	if(count == 0) {
-		arrowLeft.style.display = 'none';
-	}
-	function hideArr () {
-		sliderBtns.forEach(item => {
-			item.style.display = 'none';
+// 	if(count == 0) {
+// 		arrowLeft.style.display = 'none';
+// 	}
+// 	function hideArr () {
+// 		sliderBtns.forEach(item => {
+// 			item.style.display = 'none';
 
-		})
-		sliderContents.forEach(items => {
-			items.style.display = 'none';
-		})
+// 		})
+// 		sliderContents.forEach(items => {
+// 			items.style.display = 'none';
+// 		})
 
-	};
-	function showContentArr (i = 0) {
-		sliderContents[i].style.display = 'block';
-		sliderBtns[i].style.display = 'block';
+// 	};
+// 	function showContentArr (i = 0) {
+// 		sliderContents[i].style.display = 'block';
+// 		sliderBtns[i].style.display = 'block';
 		
-	}
-	hideArr ();
-	showContentArr(0)
+// 	}
+// 	hideArr ();
+// 	showContentArr(0)
 
-	arrowAll.forEach(item => {
-		item.addEventListener('click', (e) => {
-			hideArr ();
-			if(e.target.classList.contains('arrow-right')) {
-				count++;
-				showContentArr(count)
-				arrowAll.forEach(arrow => {
-					arrow.style.display = 'block';
-				})
-				if(count == (sliderContents.length -1)) {
-					arrowRight.style.display = 'none';
-				}
-			}
-			if(e.target.classList.contains('arrow-left')) {
-				count--;
-				showContentArr(count)
-				arrowAll.forEach(arrow => {
-					arrow.style.display = 'block';
-				})
-				if(count == 0) {
-					arrowLeft.style.display = 'none';
-				}
-			}
+// 	arrowAll.forEach(item => {
+// 		item.addEventListener('click', (e) => {
+// 			hideArr ();
+// 			if(e.target.classList.contains('arrow-right')) {
+// 				count++;
+// 				showContentArr(count)
+// 				arrowAll.forEach(arrow => {
+// 					arrow.style.display = 'block';
+// 				})
+// 				if(count == (sliderContents.length -1)) {
+// 					arrowRight.style.display = 'none';
+// 				}
+// 			}
+// 			if(e.target.classList.contains('arrow-left')) {
+// 				count--;
+// 				showContentArr(count)
+// 				arrowAll.forEach(arrow => {
+// 					arrow.style.display = 'block';
+// 				})
+// 				if(count == 0) {
+// 					arrowLeft.style.display = 'none';
+// 				}
+// 			}
 			
-		})
-	})
-}
+// 		})
+// 	})
+// }
 
 
 
 
 // Отступ блока 
 
-	const blockMargin1 = window.screen.availWidth;
-	const sliderMargin1 = document.querySelector('.slider__items');
-	sliderMargin1.style.width = blockMargin1+'px';
+	// const blockMargin1 = window.screen.availWidth;
+	// const sliderMargin1 = document.querySelector('.slider__items');
+	// sliderMargin1.style.width = blockMargin1+'px';
 
-	const blockMargin = document.querySelector('.about__items').offsetLeft;
-	sliderMargin1.style.paddingLeft = blockMargin+'px';
+	// const blockMargin = document.querySelector('.about__items').offsetLeft;
+	// sliderMargin1.style.paddingLeft = blockMargin+'px';
 
 
 
